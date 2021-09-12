a968980ed096b89b3c9db5c9f32718dde6f8f9e0
Pradeep Prajapat <er.eepradeep@gmail.com>
2021-09-12 15:09:54 +0530

    Preview working

--- src/Component/Textfield.js
+++ src/Component/Textfield.js
@@ -20,23 +20,34 @@ export default function Textfield(props) {
   const [text, setText] = useState("Enter your text here...");
   //setText("newtext");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handlOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handlOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          conert to Lowercase
+        </button>
+        <div className="container">
+          <h2>Your text summary</h2>
+          <p>
+            {text.split(" ").length} words and {text.length} Characters
+          </p>
+          <p>{0.008 * text.split(" ").length} Minutes to read</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
+        </div>
       </div>
-      <button className="btb btn primary" onClick={handleUpClick}>
-        Convert to uppercase
-      </button>
-      <button className="btn btn secondary" onClick={handleLoClick}>
-        conert to Lowercase
-      </button>
-    </div>
+    </>
   );
 }
