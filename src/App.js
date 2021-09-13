69ca7493e51208b839c4882fc0d3da69f330d53f
Pradeep Prajapat <er.eepradeep@gmail.com>
2021-09-13 22:35:06 +0530

    addint about

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Component/Navbar";
-import Textfield from "./Component/Textfield";
+//import Textfield from "./Component/Textfield";
+import about from "./Component/about";
 
 function App() {
   return (
     <>
-      {<Navbar title="TextUtills" aboutText="about Textutils" />}
+      {<Navbar title="TextUtills" aboutText="About" />}
       {/*<Navbar/>*/}
       <div className="container my-3">
-        <Textfield heading="Enter your text here" />
+        {/*<Textfield heading="Enter your text here" />*/}
         {/*<Textfield />*/}
+        <about />
       </div>
     </>
   );
---
+++ src/Component/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+      <button className="btn btn-primary mx-2">Convert to uppercase</button>
+    </div>
+  );
+}
